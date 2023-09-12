b68ca5e5a28efc55ca3918843c46b41bf5ce9a1b
moonno317 <139625145+moonno317@users.noreply.github.com>
2023-09-12 18:28:01 +0700

    Update pacman.c

--- pacman.c
+++ pacman.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <ncurses.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define BOARD_WIDTH 20
 #define BOARD_HEIGHT 10
@@ -15,12 +17,16 @@ int main() {
         "#                 #",
         "#                 #",
         "#                 #",
-        "###################", };
+        "###################" };
 
     int pacmanX = 1;
     int pacmanY = 1;
-
+    int ghostX = 10;
+    int ghostY = 5;
     int ch;
+
+    srand(time(NULL));
+
     initscr();
     keypad(stdscr, TRUE);
     noecho();
@@ -28,10 +34,11 @@ int main() {
     do {
         clear();
 
-	for (int y = 0; y < BOARD_HEIGHT; y++) {
+        for (int y = 0; y < BOARD_HEIGHT; y++) {
             printw("%s\n", board[y]); }
 
-	ch = getch();
+        ch = getch();
+
         switch (ch) {
             case KEY_UP:
                 if (board[pacmanY - 1][pacmanX] != '#') {
@@ -57,8 +64,37 @@ int main() {
                     pacmanX++; }
                 break; }
 
-		board[pacmanY][pacmanX] = 'C'; } while (ch != 'q');
+        int direction = rand() % 4;
+        switch (direction) {
+            case 0:
+                if (board[ghostY - 1][ghostX] != '#') {
+                    board[ghostY][ghostX] = ' ';
+                    ghostY--; }
+                break;
 
-    endwin();
+            case 1:
+                if (board[ghostY + 1][ghostX] != '#') {
+                    board[ghostY][ghostX] = ' ';
+                    ghostY++; }
+                break;
 
-    return 0; }
+            case 2:
+                if (board[ghostY][ghostX - 1] != '#') {
+                    board[ghostY][ghostX] = ' ';
+                    ghostX--; }
+                break;
+
+            case 3:
+                if (board[ghostY][ghostX + 1] != '#') {
+                    board[ghostY][ghostX] = ' ';
+                    ghostX++; }
+                break; }
+
+        board[pacmanY][pacmanX] = 'C';
+        board[ghostY][ghostX] = 'A'; }
+
+	while (ch != 'q');
+
+    	endwin();
+
+    	return 0; }
